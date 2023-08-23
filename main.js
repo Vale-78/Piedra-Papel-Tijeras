8cf5810d26d0888cbf3a5968ee30fed1ff1072e5
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-08-23 16:49:58 -0300

    Logré selección de usuario y mensaje sobre elección del jugador.

--- main.js
+++ main.js
@@ -1,44 +1,94 @@
+let eleccionJugador;
+let posiblesJugadas = ["piedra", "papel","tijera"];
+let piedra = posiblesJugadas[0];
+let papel = posiblesJugadas[1];
+let tijeras= posiblesJugadas[2];
 
 // disable desabilitar botones
 // display NamedNodeMap, display block
 
 
+
 function saludarUsuario() {
     let nombre = document.getElementById("nombreUsuario").value;
-    document.getElementById("saludoUsuario").innerHTML = "Bienvenido/a " + nombre + " !" + "<br/> Si estás listo para jugar, inicia Juego";
+    document.getElementById("saludoUsuario").innerHTML = "Bienvenida/o " + nombre + " !" + "<br/> Si estás lista/o para comenzar, inicia Juego!";
 }
 
 document.addEventListener("DOMContentLoaded", function (){
-        const botonComenzar = document.getElementById('startButton');
-        botonComenzar.addEventListener("click", saludarUsuario, true);
-    })
+    let botonIniciarJuego= document.getElementById("startButtonJuego");
+    botonIniciarJuego.style.display = "none";
+    /* oculto boton reiniciar juego */
+    let seccionReiniciar= document.getElementById("reiniciarJuego");
+    seccionReiniciar.style.display = "none"
+
+/* oculto iiconos para seleccionar jugada */
+    let seccionSeleccionarJugada = document.getElementById("seleccionar-jugada");
+    seccionSeleccionarJugada.style.display = "none";
+
+    const botonComenzar = document.getElementById("startButton");
+    botonComenzar.addEventListener("click", saludarUsuario, true);
+    botonComenzar.addEventListener("click", iniciarJuego, true);
+})
+
+function iniciarJuego(){
+    let botonSaludo= document.getElementById("startButton");
+    botonSaludo.style.display = "none";
+
+    let botonIniciarJuego= document.getElementById("startButtonJuego");
+    botonIniciarJuego.style.display = "block";
+   
+    botonIniciarJuego.addEventListener("click", comenzar);
+}
+
+function comenzar (){
+    let seccionSaludoInicial = document.getElementById("saludoContainer");
+    seccionSaludoInicial.style.display = "none";
+
+    let seccionSeleccionarJugada = document.getElementById("seleccionar-jugada");
+    seccionSeleccionarJugada.style.display = "block";
     
+    let botonPiedra = document.getElementById("piedra");
+    let botonPapel = document.getElementById("papel");
+    let botonTijeras = document.getElementById("tijeras");
+    botonPiedra.addEventListener("click", ()=> {
+         eleccionJugador = "piedra";
+         document.getElementById("eleccion").innerHTML = "Elegiste Piedra" });
+    botonPapel.addEventListener("click", ()=> {
+         eleccionJugador = "papel";
+         console.log(eleccionJugador);
+         document.getElementById("eleccion").innerHTML = "Elegiste Papel"});
+    botonTijeras.addEventListener("click", ()=> {
+        eleccionJugador = "tijeras";
+        document.getElementById("eleccion").innerHTML = "Elegiste Tijeras"
+        console.log(eleccionJugador);});  
+}
 
-// const piedra = "piedra";
-// const papel = "papel";
-// const tijeras = "tijeras";
-// //  en esta ocasión, el usuario ingresará directamente piedra, papel o tijera (sim distinción de mayusculas
-// //  o minúsculas) y no su n° correspondiente
 
-// function random(min, max) {
-//     return Math.floor((Math.random() * (max - min + 1)) + min);
-// }
-// let aleatorio = random(0,2);
-// let jugadaCompu;
 
-// function obtenerJugadaComputadora(){
-//     if(aleatorio==0){
-//         jugadaCompu = piedra;
-//     } else if(aleatorio==1){
-//         jugadaCompu = papel;
-//     }else {
-//         jugadaCompu = tijeras;
-//     }
-//  return jugadaCompu;
-// }
 
-// const readlineSync = require('readline-sync');
-// const eleccionJugador = readlineSync.question('Elige : piedra, papel o tijeras: ').toLowerCase();
+
+//  en esta ocasión, el usuario ingresará directamente piedra, papel o tijera (sim distinción de mayusculas
+//  o minúsculas) y no su n° correspondiente
+
+function random(min, max) {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+}
+let aleatorio = random(0,2);
+let jugadaCompu;
+
+function obtenerJugadaComputadora(){
+    if(aleatorio==0){
+        jugadaCompu = piedra;
+    } else if(aleatorio==1){
+        jugadaCompu = papel;
+    }else {
+        jugadaCompu = tijeras;
+    }
+ return jugadaCompu;
+}
+
+// const readlineSync = require("readline-sync");
+// const eleccionJugador = readlineSync.question("Elige : piedra, papel o tijeras: ").toLowerCase();
 
 // // Elimino function obtenerJugadaUsuario() ya que ahora no es necesaria.        
 

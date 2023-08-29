5d714ec4f9447fb099281887c1f1b44fc3a72583
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-08-29 18:59:20 -0300

    Modifiqué README y agregué imágenes.

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
 # Piedra-Papel-Tijeras
 Interacción entre HTML, JS y CSS.
+# PIEDRA-PAPEL-TIJERAS
+## AUTOR: Inés Valeria Foglino
+
+## Herramientas
+Este proyecto integra HTML, CSS Y JavaScript. 
+
+## Características
+
+Aquí podrás divertirte jugando a Piedra Papel y tijeras contra tu PC.
+
+* Gana el mejor de 5 jugadas.
+* Por tanto, el que gana 3 jugadas, es el campeón.
+* Los empates no se cuentan como partidos jugados.
+
+
+## Casos de prueba:
+
+#### Animación de título
+<< Al colocar el mouse sobre el título, observarás una dinámica animacíon (Rotación y cambio de colores)
+
+#### Ingreso del nombre de usuario
+<< Es necesario completar esta entrada para acceder al saludo y así habilitar el botón "Iniciar Juego"
+   Entrada1: "Nombre";   
+* Salida: "Bienvenida/o " + nombre + " !" + "<br/> Si estás lista/o para comenzar, inicia Juego!".
+
+#### Habilitación al clickear el botón "Iniciar Juego"
+<< Una vez concretado el saludo, se habilita el botón para Iniciar juego!!
+Haciendo "click" al botón "Iniciar Juego", se habilitan botones para seleccionar Jugada:
+* Piedra.
+* Papel.
+* Tijeras.
+
+#### Campos vacíos
+<< Entrada1:  ;  Entrada2: 25;  Operación: Todas; 
+* Salida: "Debes ingresar un valor en ambos campos".
+
+#### Más de un punto
+<< Entrada1: 2.345.03  ;  Entrada2: 10;  Operación: Todas; 
+* Salida: "Uno o ambos valores ingresados no son numéricos".
+
+#### Caso general
+<< Entrada1: 3;  Entrada2: 10;  Operación: División; 
+* Salida: o.3.
+
+<< Entrada1: 8;  Entrada2: 235;  Operación: Multiplicación; 
+* Salida: 1880.
+
+
+## Algoritmo principal.
+```javascript
+function calcular(){
+    (...)
+
+    if((isNaN(valorNumerico1)) || (isNaN(valorNumerico2))) {
+        alert ('Uno o ambos valores ingresados no son numéricos');
+    } else if((n1 === '') || (n2 ==='')){
+        alert ('Debes ingresar un valor en ambos campos');
+    } else{
+       (...)
+            if(select.value == "suma"){
+               let result = suma(valorNumerico1,valorNumerico2);
+               document.getElementById("resultado").innerHTML = result;
+            }else if(select.value == "resta"){
+                let result = resta(valorNumerico1,valorNumerico2);
+                document.getElementById("resultado").innerHTML = result;
+            }else if(select.value == "multiplicacion"){
+                let result = multiplicacion(valorNumerico1,valorNumerico2);
+                document.getElementById("resultado").innerHTML = result;
+            }else if(select.value == "division"){
+                let result = division(valorNumerico1,valorNumerico2);
+                if(valorNumerico2 === 0){
+                    document.getElementById("resultado").innerHTML = "La operación no es válida";
+                } else{
+                document.getElementById("resultado").innerHTML = result;
+                }
+            }               
+       (...)
+    }
+ }
+```
+
+## Cómo correr la aplicación _"Calculadora"_?
+
+Para ejecutar esta aplicación, simplemente se necesita clonar el proyecto y ejecutar el archivo html.
+Sino, accede directamente desde aquí:
+    [GitHub Pages](https://vale-78.github.io/Calculadora/).
+
+
+## Contribuciones
+
+Cualquier tipo de aportes son bienvenidos.
+
+1. <a href='https://help.github.com/articles/fork-a-repo/'>**(Fork)**</a> Bifurque el repositorio en GitHub.
+2. <a href='https://help.github.com/articles/cloning-a-repository/'>**(Clone)**</a> Clone el proyecto en su propia máquina.
+3. <a href='https://git-scm.com/book/en/v2/Git-Basics-Recording-Changes-to-the-Repository'>**(Commit)**</a> Confirmar cambios en la rama de desarrollo. <a href='https://git-scm.com/book/en/v2/Git-Branching-Branches-in-a-Nutshell'>**(development branch)**</a>.
+4. <a href='https://help.github.com/articles/pushing-to-a-remote/'>**(Push)**</a> Empuje su trabajo de regreso a su repositorio en git.
+5. <a href='https://help.github.com/articles/about-pull-requests/'>**(Pull request)**</a> Envíe una solicitud de extracción para que pueda revisar sus cambios. 
+
+## Impresión de pantalla
+
+![](./assest/MiCalcu.png)
--- main.js
+++ main.js
@@ -12,126 +12,116 @@ let partidosJugados = 0;
 // disable desabilitar botones
 // display NamedNodeMap, display block
 
-    function saludarUsuario() {
-        let nombre = document.getElementById("nombreUsuario").value;
-        let saludoANombre =document.getElementById("saludoUsuario");
-     
-        // Falta ver validacion inicial , activar boton reiniciar y lograr disable. Agregar suma empates
-        
-        if(nombre=="" || !(isNaN(nombre))){
-            saludoANombre.innerHTML = "Ingresa un nombre válido";
-            const botonComenzar = document.getElementById("startButton");
-            botonComenzar.style.display = "block";
-            const botonIniciarJuego= document.getElementById("startButtonJuego");
-            botonIniciarJuego.style.display = "none";
-        
-        }else{
-            saludoANombre.innerHTML = "Bienvenida/o " + nombre + " !" + "<br/> Si estás lista/o para comenzar, inicia Juego!"; 
-            iniciarJuego();
-        } 
-    }
-    document.addEventListener("DOMContentLoaded", function (){
-        let botonIniciarJuego= document.getElementById("startButtonJuego");
-        botonIniciarJuego.style.display = "none";
-        /* oculto boton reiniciar juego */
-        let seccionReiniciar= document.getElementById("reiniciarJuego");
-        seccionReiniciar.style.display = "none";
-
-    /* oculto iiconos para seleccionar jugada */
-        let seccionSeleccionarJugada = document.getElementById("seleccionar-jugada");
-        seccionSeleccionarJugada.style.display = "none";
-
+function saludarUsuario() {
+    let nombre = document.getElementById("nombreUsuario").value;
+    let saludoANombre =document.getElementById("saludoUsuario");
+    
+    if(nombre=="" || !(isNaN(nombre))){
+        saludoANombre.innerHTML = "Ingresa un nombre válido";
         const botonComenzar = document.getElementById("startButton");
-        botonComenzar.addEventListener("click", saludarUsuario, true);
-
-    })
-
-    
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-    }
-    
-
-    function iniciarJuego(){
-        let botonSaludo= document.getElementById("startButton");
-        botonSaludo.style.display = "none";
-
-        let botonIniciarJuego= document.getElementById("startButtonJuego");
-        botonIniciarJuego.style.display = "block";
-    
-        botonIniciarJuego.addEventListener("click", comenzar);
-    }
-
-    function comenzar (){
-       
-        let seccionSaludoInicial = document.getElementById("saludoContainer");
-        seccionSaludoInicial.style.display = "none";
-        
-        let seccionSeleccionarJugada = document.getElementById("seleccionar-jugada");
-        seccionSeleccionarJugada.style.display = "block";
-
-        let botonPiedra = document.getElementById("piedra");
-        let botonPapel = document.getElementById("papel");
-        let botonTijeras = document.getElementById("tijeras");
-
-        botonPiedra.addEventListener("click", ()=> {
-            eleccionJugador = piedra;
-            document.getElementById("eleccionJugador").innerHTML = "Elegiste Piedra 🪨"
-            //  console.log(eleccionJugador);
-            let jugadaCOMPU = JugadaComputadora();
-            determinarGanador(piedra, jugadaCOMPU);
-            });
-        botonPapel.addEventListener("click", ()=> {
-            eleccionJugador = papel;
-            document.getElementById("eleccionJugador").innerHTML = "Elegiste Papel 🧻"
-            //  console.log(eleccionJugador);
-            let jugadaCOMPU = JugadaComputadora();
-            determinarGanador(papel, jugadaCOMPU);
-            });
-            
-        botonTijeras.addEventListener("click", ()=> {
-            eleccionJugador = tijeras;
-            document.getElementById("eleccionJugador").innerHTML = "Elegiste Tijeras ✂️"
-            // console.log(eleccionJugador);
-            let jugadaCOMPU = JugadaComputadora();
-            determinarGanador(tijeras, jugadaCOMPU);
-
-        });  
-        
-        return eleccionJugador;   
-    }
-   
-
-    function JugadaComputadora(){
-        let eleccionAleatoria = getRandomInt(3);
-
-        if(eleccionAleatoria == 0){
-            jugadaCompu = piedra;
-            document.getElementById("eleccionCompu").innerHTML = "La compu eligió Piedra 🪨";
-
-        } else if(eleccionAleatoria == 1) {
-            jugadaCompu = papel;
-            document.getElementById("eleccionCompu").innerHTML = "La compu eligió Papel 🧻";
-        } else {
-            jugadaCompu = tijeras;
-            document.getElementById("eleccionCompu").innerHTML = "La compu eligió Tijeras ✂️";
-        }
-    return jugadaCompu;
+        botonComenzar.style.display = "block";
+        const botonIniciarJuego= document.getElementById("startButtonJuego");
+        botonIniciarJuego.style.display = "none";
+    }else{
+        saludoANombre.innerHTML = "Bienvenida/o " + nombre + " !" + "<br/> Si estás lista/o para comenzar, inicia Juego!"; 
+        iniciarJuego();
     } 
+}
+document.addEventListener("DOMContentLoaded", function (){
+    let botonIniciarJuego= document.getElementById("startButtonJuego");
+    botonIniciarJuego.style.display = "none";
+    /* oculto boton reiniciar juego */
+    let seccionReiniciar= document.getElementById("reiniciarJuego");
+    seccionReiniciar.style.display = "none";
 
-    function determinarGanador(eleccionJugador, jugadaCompu){
-       
-        while((ganaPC < 3 && ganaUsuario < 3) ) { 
-            const resultadoRondaParrafo = document.getElementById("resultadoRonda");
-            let numGanaUsuario = document.getElementById("numeroGanaUsuario");
-            let numGanaPC = document.getElementById("numeroGanaPC");
-            
+/* oculto iiconos para seleccionar jugada */
+    let seccionSeleccionarJugada = document.getElementById("seleccionar-jugada");
+    seccionSeleccionarJugada.style.display = "none";
+
+    const botonComenzar = document.getElementById("startButton");
+    botonComenzar.addEventListener("click", saludarUsuario, true);
+})
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function iniciarJuego(){
+    let botonSaludo= document.getElementById("startButton");
+    botonSaludo.style.display = "none";
+
+    let botonIniciarJuego= document.getElementById("startButtonJuego");
+    botonIniciarJuego.style.display = "block";
+
+    botonIniciarJuego.addEventListener("click", comenzar);
+}
+
+function comenzar (){
+    
+    let seccionSaludoInicial = document.getElementById("saludoContainer");
+    seccionSaludoInicial.style.display = "none";
+    
+    let seccionSeleccionarJugada = document.getElementById("seleccionar-jugada");
+    seccionSeleccionarJugada.style.display = "block";
+
+    let botonPiedra = document.getElementById("piedra");
+    let botonPapel = document.getElementById("papel");
+    let botonTijeras = document.getElementById("tijeras");
+
+    botonPiedra.addEventListener("click", ()=> {
+        eleccionJugador = piedra;
+        document.getElementById("eleccionJugador").innerHTML = "Elegiste Piedra 🪨"
+        //  console.log(eleccionJugador);
+        let jugadaCOMPU = JugadaComputadora();
+        determinarGanador(piedra, jugadaCOMPU);
+        });
+    botonPapel.addEventListener("click", ()=> {
+        eleccionJugador = papel;
+        document.getElementById("eleccionJugador").innerHTML = "Elegiste Papel 🧻"
+        //  console.log(eleccionJugador);
+        let jugadaCOMPU = JugadaComputadora();
+        determinarGanador(papel, jugadaCOMPU);
+        });
+        
+    botonTijeras.addEventListener("click", ()=> {
+        eleccionJugador = tijeras;
+        document.getElementById("eleccionJugador").innerHTML = "Elegiste Tijeras ✂️"
+        // console.log(eleccionJugador);
+        let jugadaCOMPU = JugadaComputadora();
+        determinarGanador(tijeras, jugadaCOMPU);
+    });  
+    return eleccionJugador;   
+}
+
+function JugadaComputadora(){
+    let eleccionAleatoria = getRandomInt(3);
+
+    if(eleccionAleatoria == 0){
+        jugadaCompu = piedra;
+        document.getElementById("eleccionCompu").innerHTML = "La compu eligió Piedra 🪨";
+
+    } else if(eleccionAleatoria == 1) {
+        jugadaCompu = papel;
+        document.getElementById("eleccionCompu").innerHTML = "La compu eligió Papel 🧻";
+    } else {
+        jugadaCompu = tijeras;
+        document.getElementById("eleccionCompu").innerHTML = "La compu eligió Tijeras ✂️";
+    }
+return jugadaCompu;
+} 
+
+function determinarGanador(eleccionJugador, jugadaCompu){
+    
+    while((ganaPC < 3 && ganaUsuario < 3)) { 
+    const resultadoRondaParrafo = document.getElementById("resultadoRonda");
+    let numGanaUsuario = document.getElementById("numeroGanaUsuario");
+    let numGanaPC = document.getElementById("numeroGanaPC");
+        
         if(eleccionJugador==jugadaCompu){
             resultado = "Empate";
             resultadoRondaParrafo.innerHTML = resultado;
             numGanaUsuario.innerHTML = ganaUsuario;
-            numGanaPC.innerHTML= ganaPC;
-            
+            numGanaPC.innerHTML= ganaPC;            
         }else if((eleccionJugador==piedra && jugadaCompu ==tijeras) || (eleccionJugador==papel && jugadaCompu == piedra ) || (eleccionJugador==tijeras && jugadaCompu == papel)){
             partidosJugados = partidosJugados +1;
             console.log(partidosJugados);
@@ -149,36 +139,34 @@ let partidosJugados = 0;
             numGanaUsuario.innerHTML = ganaUsuario;
             numGanaPC.innerHTML= ganaPC;
         }  
-        return resultado;
-     }
-     let usuGana= ganaUsuario;
-     let compuGana= ganaPC
-      resultadoFinalfin( usuGana, compuGana);
+    return resultado;
     }
+    let usuGana= ganaUsuario;
+    let compuGana= ganaPC
+    resultadoFinalfin( usuGana, compuGana);
+}
 
+function resultadoFinalfin( ganaUsuario, ganaPC){
+    let nombre = document.getElementById("nombreUsuario").value;
+    let resultFinal = document.getElementById("resultFinal");
 
-    function resultadoFinalfin( ganaUsuario, ganaPC){
-
-        let nombre = document.getElementById("nombreUsuario").value;
-        let resultFinal = document.getElementById("resultFinal");
-        
-        if( ganaUsuario > ganaPC){
-            resultFinal.innerHTML = `Felicitaciones ` + nombre + `,  Ganaste!!!!`
-            document.getElementById("Piedra").disabled;
-            document.getElementById("Papel").disabled;
-            document.getElementById("Tijeras").disabled;
-        }else{
-            resultFinal.innerHTML = ` Lo siento `  + nombre + `, Gana la Computadora.`
-            document.getElementById("Piedra").disabled;
-            document.getElementById("Papel").disabled;
-            document.getElementById("Tijeras").disabled;
-        };
-        let seccionReiniciar= document.getElementById("reiniciarJuego");
-        seccionReiniciar.style.display = "block";      
-        let resetJuego = document.getElementById("resetButton");
-        resetJuego.addEventListener("click",reiniciar);
-        return resultFinal;
-    }            
+    if( ganaUsuario > ganaPC){
+        resultFinal.innerHTML = `Felicitaciones ` + nombre + `,  Ganaste!!!!`
+        // document.getElementById("Piedra").disabled = true;
+        // document.getElementById("Papel").disabled = true;
+        // document.getElementById("Tijeras").disabled = true;
+    }else{
+        resultFinal.innerHTML = ` Lo siento `  + nombre + `, Gana la Computadora.`
+        // document.getElementById("Piedra").disabled = true;
+        // document.getElementById("Papel").disabled = true;
+        // document.getElementById("Tijeras").disabled = true;
+    };
+    let seccionReiniciar= document.getElementById("reiniciarJuego");
+    seccionReiniciar.style.display = "block";      
+    let resetJuego = document.getElementById("resetButton");
+    resetJuego.addEventListener("click",reiniciar);
+    return resultFinal;
+}            
 
 function reiniciar(){
 resultado = "";
